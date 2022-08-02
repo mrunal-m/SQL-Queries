8404bc06e460f17b3ef394fec281cc178f0e3e24
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2022-08-02 18:17:50 +0530

    Update video_play_distribution.sql

--- Data Quality of Events/video_play_distribution.sql
+++ Data Quality of Events/video_play_distribution.sql
@@ -1,6 +1,9 @@
 
 (select date(time), 
 "duration" as column,
+COUNT (*) as total_rows,
+COUNT (Case when duration IS NULL then 1 end) as null_count,
+COUNT (case when duration IS NOT NULL then 1 end) unique,
 MIN(duration) as minimum,
   approx_quantiles(duration, 100)[SAFE_ORDINAL(5)] as p5,
   approx_quantiles(duration, 100)[SAFE_ORDINAL(25)] as p25,
@@ -10,7 +13,7 @@ MIN(duration) as minimum,
   approx_quantiles(duration, 100)[SAFE_ORDINAL(95)] as p95,
   approx_quantiles(duration, 100)[SAFE_ORDINAL(99)] as p99,
   approx_quantiles(duration, 100)[SAFE_ORDINAL(100)] as maximimum
-  from `maximal-furnace-783.sc_analytics.video_play` -- Change to moj_analytics for Moj
+  from `maximal-furnace-783.sc_analytics.video_play`
   WHERE
 time>='2022-07-25' AND time<="2022-07-31"
 GROUP BY 1
@@ -19,6 +22,9 @@ GROUP BY 1
 UNION ALL
 (select date(time), 
 "timeSpent" as column,
+COUNT (*) as total_rows,
+COUNT (Case when duration IS NULL then 1 end) as null_count,
+COUNT (case when duration IS NOT NULL then 1 end) unique,
 MIN(SAFE_CAST(timeSpent AS INT64)) as minimum,
   approx_quantiles(SAFE_CAST(timeSpent AS INT64), 100)[SAFE_ORDINAL(5)] as p5,
   approx_quantiles(SAFE_CAST(timeSpent AS INT64), 100)[SAFE_ORDINAL(25)] as p25,
@@ -37,6 +43,9 @@ GROUP BY 1
 UNION ALL
 (select date(time), 
 "percentage" as column,
+COUNT (*) as total_rows,
+COUNT (Case when percentage IS NULL then 1 end) as null_count,
+COUNT (case when percentage IS NOT NULL then 1 end) unique,
 MIN(percentage) as minimum,
   approx_quantiles(percentage, 100)[SAFE_ORDINAL(5)] as p5,
   approx_quantiles(percentage, 100)[SAFE_ORDINAL(25)] as p25,
@@ -55,6 +64,9 @@ GROUP BY 1
 UNION ALL
 (select date(time), 
 "percentageFloat" as column,
+COUNT (*) as total_rows,
+COUNT (Case when percentageFloat IS NULL then 1 end) as null_count,
+COUNT (case when percentageFloat IS NOT NULL then 1 end) unique,
 MIN(percentageFloat) as minimum,
   approx_quantiles(percentageFloat, 100)[SAFE_ORDINAL(5)] as p5,
   approx_quantiles(percentageFloat, 100)[SAFE_ORDINAL(25)] as p25,
@@ -74,6 +86,9 @@ GROUP BY 1
 UNION ALL
 (select date(time), 
 "videoStartTime" as column,
+COUNT (*) as total_rows,
+COUNT (Case when videoStartTime IS NULL then 1 end) as null_count,
+COUNT (case when videoStartTime IS NOT NULL then 1 end) unique,
 MIN(videoStartTime) as minimum,
   approx_quantiles(videoStartTime, 100)[SAFE_ORDINAL(5)] as p5,
   approx_quantiles(videoStartTime, 100)[SAFE_ORDINAL(25)] as p25,
