39ea159ba89d73f93c860b2c229ed03e93dad8c9
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2022-08-10 11:22:01 +0530

    Update tables_referencing_firebase_tables.sql

--- Information Schema tables/tables_referencing_firebase_tables.sql
+++ Information Schema tables/tables_referencing_firebase_tables.sql
@@ -3,7 +3,9 @@ FROM
   `region-us`.INFORMATION_SCHEMA.JOBS_BY_ORGANIZATION
   , UNNEST(referenced_tables) as referenced_tables
 WHERE
-  EXTRACT(DATE FROM  creation_time) >="2022-06-01"
+  EXTRACT(DATE FROM  creation_time) >="2022-08-01"
   AND referenced_tables.dataset_id LIKE "%analytics_163194662%"
+  AND destination_table.table_id NOT LIKE "%session_time_hourly%"
+  AND user_email NOT LIKE "%mrunal%" AND user_email NOT LIKE "%riyagupta%"
 ORDER BY
   total_bytes_billed DESC;
