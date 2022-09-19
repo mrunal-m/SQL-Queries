b06961c1f7a51b8f1e3e408cbf8dd63239e7690f
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2022-09-19 11:05:36 +0530

    Update visitor_count_revisiting_in_specific_time_period.sql

--- DAU drops Aug/visitor_count_revisiting_in_specific_time_period.sql
+++ DAU drops Aug/visitor_count_revisiting_in_specific_time_period.sql
@@ -1,29 +1,28 @@
 --DAU drop check for specific time period
---Lets say on 26th Aug 20:45-21:45 (PM) is issue time (visitor count A)
---Observed on 27th Aug 10:00 AM
---What % of A, comes back from 26 Aug 21:45 to 27 Aug 10:00 
-SELECT COUNT (distinct visiting_back) from (SELECT distinct deviceID as visiting_back 
-from maximal-furnace-783.moj_analytics.home_opened
-WHERE time(time, "Asia/Kolkata") BETWEEN time(datetime "2022-08-23 14:20:00.000000") AND time(datetime"2022-08-23 23:59:59.000000")
-AND (date(time, "Asia/Kolkata") IN ("2022-08-23"))
-AND (lower(tenant) like 'moj' or tenant is null) AND (lower(clientType) like 'android' or clientType is null )
+--Lets say on 15th Sept 6:30-9:00 (AM) is issue time (visitor count A)
+--Observed on 16th Sept 12:30 PM
+--What % of A, comes back from 15th Sept 9:00 AM to 16th Sept 12:30 PM
+ 
+SELECT COUNT (distinct visiting_back) from 
+(
+(SELECT distinct deviceID as visiting_back 
+from maximal-furnace-783.sc_analytics.home_opened
+WHERE time(time, "Asia/Kolkata") BETWEEN time(datetime "2022-09-15 09:00:00.000000") AND time(datetime"2022-09-15 23:59:59.000000")
+AND (date(time, "Asia/Kolkata") IN ("2022-09-15"))
 AND deviceId IN
 (SELECT distinct deviceId AS DAU 
-from maximal-furnace-783.moj_analytics.home_opened
-WHERE (time(time, "Asia/Kolkata") BETWEEN time(datetime "2022-08-23 13:26:00.000000") AND time(datetime"2022-08-23 14:20:00.000000"))
-AND date(time, "Asia/Kolkata")="2022-08-23"
-AND (lower(tenant) like 'moj' or tenant is null) AND (lower(clientType) like 'android' or clientType is null ))
+from maximal-furnace-783.sc_analytics.home_opened
+WHERE (time(time, "Asia/Kolkata") BETWEEN time(datetime "2022-09-15 06:30:00.000000") AND time(datetime"2022-09-15 09:00:00.000000"))
+AND date(time, "Asia/Kolkata")="2022-09-15"))
 
 UNION ALL
 (SELECT distinct deviceID as visiting_back 
-from maximal-furnace-783.moj_analytics.home_opened
-WHERE time(time, "Asia/Kolkata") BETWEEN time(datetime "2022-08-27 00:00:00.000000") AND time(datetime"2022-08-27 23:59:59.000000")
-AND (date(time, "Asia/Kolkata") IN ("2022-08-24"))
-AND (lower(tenant) like 'moj' or tenant is null) AND (lower(clientType) like 'android' or clientType is null )
+from maximal-furnace-783.sc_analytics.home_opened
+WHERE time(time, "Asia/Kolkata") BETWEEN time(datetime "2022-09-16 00:00:00.000000") AND time(datetime"2022-09-16 12:30:59.000000")
+AND (date(time, "Asia/Kolkata") IN ("2022-09-16"))
 AND deviceId IN
 (SELECT distinct deviceId AS DAU 
-from maximal-furnace-783.moj_analytics.home_opened
-WHERE (time(time, "Asia/Kolkata") BETWEEN time(datetime "2022-08-23 13:26:00.000000") AND time(datetime"2022-08-23 14:20:00.000000"))
-AND date(time, "Asia/Kolkata")="2022-08-23"
-AND (lower(tenant) like 'moj' or tenant is null) AND (lower(clientType) like 'android' or clientType is null ))
-))
+from maximal-furnace-783.sc_analytics.home_opened
+WHERE (time(time, "Asia/Kolkata") BETWEEN time(datetime "2022-09-15 06:30:00.000000") AND time(datetime"2022-09-15 09:00:00.000000"))
+AND date(time, "Asia/Kolkata")="2022-09-15"))
+)
