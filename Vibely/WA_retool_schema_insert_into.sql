0cf43a0a27fc163c4a9cd63cc20561f5564a2b0e
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2025-04-21 16:14:56 +0530

    Create WA_retool_schema_insert_into.sql

---
+++ Vibely/WA_retool_schema_insert_into.sql
@@ -0,0 +1,6 @@
+INSERT INTO `maximal-furnace-783.vibely_analytics.crmVibelyAdhocRetoolWA_feed_down_20Apr`
+
+SELECT current_timestamp() AS date, 'adhoc' Cohort,  'English' language, '' userId, '9657295754' phoneNo, 'name' userName ,
+'vibely_techissue_users_20apr25' template,
+concat('{"header" : ["','https://cdn-sc-g.sharechat.com/33d5318_1c8/tools/2945ed1e_1730205268033_sc.png','"]}')   as templateVariables,
+'https://cdn-sc-g.sharechat.com/33d5318_1c8/tools/2945ed1e_1730205268033_sc.png' cdnUrl
