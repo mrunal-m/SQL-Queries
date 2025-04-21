b5892a383847f6f313fe3965c427c53c4d57ae83
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2025-04-21 17:56:14 +0530

    Create IPL_post_match.sql

---
+++ Vibely/IPL_post_match.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE TABLE `maximal-furnace-783.vibely_analytics.crmVibelyAdhocRetool_IPL_23Apr_postmatch`
+AS (
+with a as (
+SELECT distinct SAFE_CAST(a.distinct_id AS STRING) userId, b.phoneNo, b.name userName, b.language,
+ 'https://cdn-sc-g.sharechat.com/33d5318_1c8/tools/1fb57083_1729256150911_sc.png' thumbnailUrl, 
+'Upset or Epic? 🏏' title, 'Call a friend and discuss every ball' text
+from `maximal-furnace-783.vibely_analytics.home_opened` a
+INNER JOIN `maximal-furnace-783.sc_analytics.user`b ON
+a.distinct_id = SAFE_CAST(b.id AS INT64)
+WHERE date(a.time, "Asia/Kolkata") >= "2025-04-16" AND b.tenant = 'fz'
+),
+
+b as (select * from  `maximal-furnace-783.vibely_analytics.crmNotificationsWithTemplateFinal`
+limit 1 ),
+f as(
+select date, 
+extract(hour from current_time('Asia/Kolkata')) hour,
+a.userId,
+a.userName,
+a.phoneNo,
+callsLifetime,
+notificationsSent,
+'feed' as target,
+'adHoc' cohort,
+'AdhocRetoolIPL_23Apr_postmatch' as templateId,
+a.title, 
+a.text, 
+a.thumbnailUrl, 
+notifRank, 
+setRank,
+setMap,
+a.language,
+concat('{"userName":','"', a.userName,'"', '}') as templateVariables,
+rowNumber,row_number() over(partition by a.userId order by rand())rn 
+from b left join a on true
+--left join `maximal-furnace-783.vibely_analytics.crmNotificationTemplates` t on t.state='offer' and t.templateId like '%tier_drop%'
+qualify rn=1)
+select *except(rowNumber,rn),row_number() over ()rowNumber from f
+)
