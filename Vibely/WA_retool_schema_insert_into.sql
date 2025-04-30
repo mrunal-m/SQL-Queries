f4a5ce327b3b3a9b9f05a1efd9d477347fcce28f
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2025-04-30 17:37:07 +0530

    Update WA_retool_schema_insert_into.sql

--- Vibely/WA_retool_schema_insert_into.sql
+++ Vibely/WA_retool_schema_insert_into.sql
@@ -1,10 +1,11 @@
 -- WARNING!!! date should be in timestamp() type but without UTC part
-CREATE TABLE `maximal-furnace-783.vibely_analytics.crmVibelyAdhocRetoolWA_feed_down_20Apr`
+CREATE OR REPLACE TABLE `maximal-furnace-783.vibely_analytics.crmVibelyAdhocRetoolWA_feed_down_20Apr`
   LIKE `maximal-furnace-783.vibely_analytics.crmDataWithTemplate`;
 
 INSERT INTO `maximal-furnace-783.vibely_analytics.crmVibelyAdhocRetoolWA_feed_down_20Apr`
 
 SELECT current_timestamp() AS date, 'adhoc' Cohort,  'English' language, '' userId, '9657295754' phoneNo, 'name' userName ,
-'vibely_techissue_users_20apr25' template,
+'vibely_techissue_users_20apr25' template, -- Joseph provides
 concat('{"header" : ["','https://cdn-sc-g.sharechat.com/33d5318_1c8/tools/2945ed1e_1730205268033_sc.png','"]}')   as templateVariables,
 'https://cdn-sc-g.sharechat.com/33d5318_1c8/tools/2945ed1e_1730205268033_sc.png' cdnUrl
+--cdn is provided by Design
