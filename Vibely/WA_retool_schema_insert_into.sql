3eb1fb7684d5c8411fabb6fd8dcdd30362b4f361
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2025-04-30 17:21:11 +0530

    Update WA_retool_schema_insert_into.sql

--- Vibely/WA_retool_schema_insert_into.sql
+++ Vibely/WA_retool_schema_insert_into.sql
@@ -1,3 +1,5 @@
+-- WARNING!!! date should be in timestamp() type but without UTC part
+
 INSERT INTO `maximal-furnace-783.vibely_analytics.crmVibelyAdhocRetoolWA_feed_down_20Apr`
 
 SELECT current_timestamp() AS date, 'adhoc' Cohort,  'English' language, '' userId, '9657295754' phoneNo, 'name' userName ,
