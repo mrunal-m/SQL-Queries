82dfc59044c9ce10f3d86503d6615805b18dc93b
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2025-06-25 13:17:57 +0530

    Update exp_tofu.sql

--- Vibely/AV switch/exp_tofu.sql
+++ Vibely/AV switch/exp_tofu.sql
@@ -1,20 +1,26 @@
 -- checking exp variant wise top of the funnel
-WITH 
-exp_base AS 
+--tofu
+WITH exp_base AS 
 (SELECT DISTINCT userId, variant FROM `sharechat-production.experimentationV2.abTest_view_events_backendV2` 
-WHERE expID =  "9141cd90-998c-4cbf-af86-b8d9580ba381" AND 
-DATE(timestamp,"Asia/Kolkata") >= "2025-06-04"
-AND timestamp(timestamp)>=TIMESTAMP('2025-06-04 06:30:00 UTC')
+WHERE expID =  "91c2ff6e-6364-41cb-b307-4258aaf0ff0e" AND 
+DATE(timestamp,"Asia/Kolkata") >= "2025-06-10"
+AND timestamp(timestamp)>=TIMESTAMP('2025-06-10 13:30:00 UTC')
 AND version NOT IN ('NA') AND version > '0'),
 
-t1 AS (SELECT DISTINCT consultee_user_id userId, consultant_user_id hostId, consultation_id, media_type
-FROM `maximal-furnace-783.sc_analytics.consultation`
+e AS 
+(SELECT variant, COUNT(distinct userId) users from exp_base
+GROUP BY 1),
+
+t1 AS (SELECT DATE(time, "Asia/Kolkata") dt, --exp_base.variant, exp_base.userId user,
+consultee_user_id userId, consultant_user_id hostId, consultation_id, media_type
+FROM  `maximal-furnace-783.sc_analytics.consultation` c
+
 WHERE consultation_type = 'FIND_A_FRIEND' AND tenant = 'fz' AND status = 'completed'
-AND date(time, "Asia/Kolkata") >= "2025-06-04" AND timestamp(time)>=TIMESTAMP('2025-06-04 06:30:00 UTC')
+AND date(time, "Asia/Kolkata") >= "2025-06-10" AND timestamp(time)>=TIMESTAMP('2025-06-10 13:30:00 UTC')
 ),
 
 t2 AS (
-SELECT DISTINCT userId, hostId,  consultation_id,
+SELECT dt, userId, hostId,  consultation_id,
 FROM t1 WHERE LOWER(media_type) LIKE "%audio%"),
 
 host AS 
@@ -26,18 +32,20 @@ WHERE date(time, "Asia/Kolkata") >= "2025-05-01"
 GROUP BY 1  HAVING appV >= 251301),
 
 t3 AS (
-SELECT t2.userId, t2.hostId, t2.consultation_id FROM 
+SELECT t2.dt, t2.userId, t2.hostId, t2.consultation_id FROM 
 t2 INNER JOIN host ON t2.hostId = host.hostId),
 
 switchClick AS (
-SELECT DISTINCT  SUBSTRING(av.userId, 2) userId, callId, 
+SELECT SUBSTRING(av.userId, 2) userId, callId, 
 FROM `maximal-furnace-783.vibely_analytics.call_av_switch_event` av 
-WHERE date(time, "Asia/Kolkata") >= "2025-06-04" AND timestamp(time)>=TIMESTAMP('2025-06-04 06:30:00 UTC')
+WHERE date(time, "Asia/Kolkata") >= "2025-06-10" AND timestamp(time)>=TIMESTAMP('2025-06-10 13:30:00 UTC')
 AND mediaType = 'audio' AND action IN ('videoSwitchButtonClicked') AND avSwitchEligibleId IS NOT NULL
 )
 
-SELECT exp_base.variant, 
-COUNT(distinct exp_base.userId) expUsers, 
+SELECT 
+t1.dt,
+exp_base.variant,
+e.users expUsers, 
 COUNT(DISTINCT t1.userId) Callers,
 COUNT(DISTINCT t1.consultation_id) TotalCalls,
 COUNT(DISTINCT t2.consultation_id) AudioCalls,
@@ -45,9 +53,13 @@ COUNT(DISTINCT t3.consultation_id) AudioCallsWithEligibleHosts,
 COUNT(DISTINCT switchClick.callId) switchClicked
 
 FROM exp_base 
-
+LEFT JOIN e On exp_base.variant = e.variant
 LEFT JOIN t1 ON exp_base.userId = t1.userId
-LEFT JOIN t2 ON t1.userId = t2.userId AND t1.consultation_id = t2.consultation_id
-LEFT JOIN t3 ON t2.hostId = t3.hostId AND t2.consultation_id = t3.consultation_id
-LEFT JOIN switchClick ON t3.consultation_id = switchClick.callid
+LEFT JOIN t2 ON t1.userId = t2.userId AND t1.consultation_id = t2.consultation_id AND t1.dt = t2.dt
+LEFT JOIN t3 ON t2.hostId = t3.hostId AND t2.consultation_id = t3.consultation_id AND t2.dt = t3.dt
+LEFT JOIN switchClick ON t1.consultation_id = switchClick.callId
+
+WHERE t1.dt IS NOT NULL 
 GROUP BY ALL
+
+ORDER BY 1 DESC, 2 ASC
