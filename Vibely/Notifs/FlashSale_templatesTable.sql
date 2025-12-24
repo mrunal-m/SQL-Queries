9ca8858528de9222337b86a77c0afb3779c98ed0
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2025-12-24 17:30:27 +0530

    Create FlashSale_templatesTable.sql

---
+++ Vibely/Notifs/FlashSale_templatesTable.sql
@@ -0,0 +1,2 @@
+CREATE OR REPLACE TABLE `maximal-furnace-783.vibely_analytics.crmFlashSaleNotificationTemplatesTable`
+AS (SELECT * FROM maximal-furnace-783.vibely_analytics.crmFlashSaleNotificationTemplates)
