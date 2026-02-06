b6f5397185f6e112ef6136b1d0661df6bbcb4920
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2026-02-06 19:54:44 +0530

    Create Adhoc_valentines_week_query.sql

---
+++ Vibely/Notifs/Adhoc_valentines_week_query.sql
@@ -0,0 +1,39 @@
+CREATE OR REPLACE TABLE `maximal-furnace-783.vibely_analytics.crmVibelyAdhocRetool_Valenties_ChocDay1`
+AS (
+with a as (
+SELECT distinct SAFE_CAST(a.distinct_id AS STRING) userId, b.phoneNo, b.name userName, b.language,
+ 'https://cdn-sc-g.sharechat.com/33d5318_1c8/56a7239_1770384122941_sc.png' thumbnailUrl, 
+'Chocolate Day special 🍫' title, 'Celebrate with sweet new gifts' text
+from `maximal-furnace-783.vibely_analytics.home_opened` a
+INNER JOIN `maximal-furnace-783.sc_analytics.user`b ON
+a.distinct_id = SAFE_CAST(b.id AS INT64)
+WHERE date(a.time, "Asia/Kolkata") >= "2025-12-01" AND b.tenant = 'fz'
+),
+
+b as (select * from  `maximal-furnace-783.vibely_analytics.crmNotificationsWithTemplateFinal`
+limit 1 ),
+f as(
+select date, 
+extract(hour from current_time('Asia/Kolkata')) hour,
+a.userId,
+a.userName,
+a.phoneNo,
+callsLifetime,
+notificationsSent,
+'feed' as target,
+'adHoc' cohort,
+'AdhocRetool_Valenties_ChocDay1' as templateId,
+a.title, 
+a.text, 
+a.thumbnailUrl, 
+notifRank, 
+setRank,
+setMap,
+a.language,
+concat('{"userName":','"', a.userName,'"', '}') as templateVariables,
+rowNumber,row_number() over(partition by a.userId order by rand())rn 
+from b left join a on true
+--left join `maximal-furnace-783.vibely_analytics.crmNotificationTemplates` t on t.state='offer' and t.templateId like '%tier_drop%'
+qualify rn=1)
+select *except(rowNumber,rn),row_number() over ()rowNumber from f
+)
