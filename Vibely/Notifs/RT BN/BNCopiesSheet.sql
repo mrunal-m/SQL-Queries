2e65f25a10872568e19bf7f68730883019b67d4f
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2026-02-20 13:05:39 +0530

    Add image link comment to BNCopiesSheet.sql
    Added a comment with a link to an image.

--- Vibely/Notifs/RT BN/BNCopiesSheet.sql
+++ Vibely/Notifs/RT BN/BNCopiesSheet.sql
@@ -1,3 +1,4 @@
+-- sheet https://cdn-sc-g.sharechat.com/33d5318_1c8/1606b910_1771512238598_sc.png
 TRUNCATE TABLE maximal-furnace-783.vibely_analytics.crmBehaviouralNotificationCohortConfig;
 INSERT INTO maximal-furnace-783.vibely_analytics.crmBehaviouralNotificationCohortConfig
 (
