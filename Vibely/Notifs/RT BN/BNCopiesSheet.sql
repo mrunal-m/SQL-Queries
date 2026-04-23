4ed921e79f7c53e2fa27cbcc1a7a05a80f69c797
Mrunal Mendgudle <90619741+mrunal-m@users.noreply.github.com>
2026-04-23 14:55:59 +0530

    Update BNCopiesSheet.sql

--- Vibely/Notifs/RT BN/BNCopiesSheet.sql
+++ Vibely/Notifs/RT BN/BNCopiesSheet.sql
@@ -1,4 +1,4 @@
--- sheet https://cdn-sc-g.sharechat.com/33d5318_1c8/1606b910_1771512238598_sc.png
+-- sheet https://docs.google.com/spreadsheets/d/1h13Rc9bZLScryTdjx4khjnfQz5VrGRIWlvi_yW05qJg/edit?gid=1226323505#gid=1226323505
 TRUNCATE TABLE maximal-furnace-783.vibely_analytics.crmBehaviouralNotificationCohortConfig;
 INSERT INTO maximal-furnace-783.vibely_analytics.crmBehaviouralNotificationCohortConfig
 (
